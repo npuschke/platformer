8cb0ff02c6ee2ddbe72f435193860953c01391fd
npuschke <niels.puschke@gmail.com>
2014-05-07 21:05:41 +0200

    restructured solution

---
+++ Tools/tools/Singleton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sept.tools
+{
+    public class Singleton
+    {
+        public static const Singleton instance = new Singleton();
+
+        private Singleton()
+        {
+
+        }
+
+        public void makeGame()
+        {
+
+        }
+    }
+}
